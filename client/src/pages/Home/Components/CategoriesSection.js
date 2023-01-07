2830ad4618b2a736d724c0c0e83d3b1af164b6d5
kobir1989 <kobir.h.ritu@gmail.com>
2023-01-07 16:57:41 +0600

    UseHttpHook added

--- client/src/pages/Home/Components/CategoriesSection.js
+++ client/src/pages/Home/Components/CategoriesSection.js
@@ -1,45 +1,36 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import styles from "../styles/CategoriesSection.module.scss";
 import Typography from "../../../components/Common/Typography/Typography";
 import CategoryCard from './CategoryCard';
 import Error500 from '../../../components/Common/Error/Error500';
-// import { useFetch } from "../../../hooks/useFetch";
-import axios from "axios";
+import { useHttpHook } from "../../../hooks/useHttpHook";
 
-
-//TODO: Category data willbe fetch from database 
-// const BASE_URL = process.env.REACT_APP_BASE_URL;
 const CategoriesSection = () => {
-   // const [data, setData] = useState([])
-   // const [error, setError] = useState(null)
-   // const [loading, setLoading] = useState(false)
-   // const getData = async () => {
-   //    try {
-   //       setLoading(true)
-   //       const response = await axios.get(`${BASE_URL}/categories/all`)
-   //       setData(response.data.allCategories)
-   //    } catch (err) {
-   //       setError(err)
-   //    } finally {
-   //       setLoading(false)
-   //    }
-   // }
-   // useEffect(() => {
-   //    getData();
-   // }, [])
-   // console.log(data)
+   const [apiData, setApiData] = useState([]);
+   const {
+      isLoading,
+      error,
+      sendRequest
+   } = useHttpHook();
+
+   useEffect(() => {
+      const getData = (data) => {
+         setApiData(data.allCategories)
+      };
+      sendRequest({ url: "categories/all", method: "get" }, getData)
+   }, [sendRequest])
+
    return (
       <div className={styles.category_wrapper}>
-         {/* {data.map((category) => (
+         {apiData.map((category) => (
             <div className={styles.card_wrapper} key={category?._id}>
                <CategoryCard
                   title={category?.name}
-                  imgSize={"big"}
                   imgUrl={category?.image}
                />
             </div>
          ))}
-         {error && <Error500 />} */}
+         {error && <Error500 />}
       </div>
    )
 }
